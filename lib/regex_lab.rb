09a651fa2930cbd51480b46222bfa8503e7480af
Cody Frank <cody.frank30@gmail.com>
2019-04-21 23:44:38 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone =~ (/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
+  phone =~ (/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/) ? true : false
 end
+
+^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$
+
+([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)
